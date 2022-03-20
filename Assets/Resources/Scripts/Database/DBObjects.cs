255669816a0009cb36b71218ca75dd7325ab70a9
Berk Algül <berkalgull@gmail.com>
2022-03-20 21:14:15 +0300

    DBObjects init and script project folder fix

---
+++ Assets/Resources/Scripts/Database/DBObjects.cs
@@ -0,0 +1,67 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DBObjects : MonoBehaviour
+{
+    [Serializable]
+    public class DeviceCurrent
+    {
+        public int levelNo;
+        public float totalPoint;
+
+        public static DeviceCurrent Default()
+        {
+            return new DeviceCurrent()
+            {
+                levelNo = 1,
+                totalPoint = 0
+            };
+        }
+    }
+
+    [Serializable]
+    public class DeviceMain
+    {
+        public int width;
+        public int height;
+        public string language;
+        public string type;
+
+        public static DeviceMain GetMain()
+        {
+            return new DeviceMain()
+            {
+                width = UnityEngine.Screen.width,
+                height = UnityEngine.Screen.height,
+                language = UnityEngine.Application.systemLanguage.ToString(),
+                type = DeviceHelper.GetDeviceType()
+            };
+        }
+    }
+
+    [Serializable]
+    public class DeviceLevel
+    {
+        public int point;
+        public int tryCount;
+        public object startDate;
+        public object endDate;
+
+        public static DeviceLevel Default()
+        {
+            return new DeviceLevel()
+            {
+                tryCount = 1,
+                startDate = Firebase.Database.ServerValue.Timestamp
+            };
+        }
+
+        public void EndLevel(int point)
+        {
+            this.point = point;
+            this.endDate = Firebase.Database.ServerValue.Timestamp;
+        }
+    }
+}
---
+++ Assets/Resources/Scripts/Helper/DeviceHelper.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeviceHelper : MonoBehaviour
+{
+    public static string GetDeviceId()
+    {
+        if (IsAndroid())
+        {
+            var clsUnity = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+            var objActivity = clsUnity.GetStatic<AndroidJavaObject>("currentActivity");
+            var objResolver = objActivity.Call<AndroidJavaObject>("getContentResolver");
+            var clsSecure = new AndroidJavaClass("android.provider.Settings$Secure");
+            return clsSecure.CallStatic<string>("getString", objResolver, "android_id");
+        }
+        else
+        {
+            return SystemInfo.deviceUniqueIdentifier;
+            // return  UnityEngine.iOS.Device.vendorIdentifier;
+        }
+    }
+
+    public static string GetDeviceType()
+    {
+#if UNITY_ANDROID && !UNITY_EDITOR
+        return "ANDROID";
+#elif UNITY_IPHONE
+        return "IOS";
+#else
+        return "EDITOR";
+#endif
+    }
+
+    public static void Vibrate(long ms = 250, bool overridePermission = false)
+    {
+        if (!overridePermission && Helper.DeviceCache.GetOptionVibration() == 0)
+            return;
+
+        if (IsAndroid())
+        {
+            var unityPlayer = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+            var currentActivity = unityPlayer.GetStatic<AndroidJavaObject>("currentActivity");
+            var vibrator = currentActivity.Call<AndroidJavaObject>("getSystemService", "vibrator");
+            vibrator.Call("vibrate", ms);
+        }
+        else
+            Handheld.Vibrate();
+
+    }
+
+    private static bool IsAndroid()
+    {
+#if UNITY_ANDROID && !UNITY_EDITOR
+            return true;
+#else
+        return false;
+#endif
+
+    }
+}
